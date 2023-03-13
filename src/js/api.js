465f819a6ac6360e60e612d5be67095b5a031b50
Marcus Reineck <100363230+MarcusRei@users.noreply.github.com>
2023-03-13 15:16:39 +0100

    fix spelling, add import, fix loop

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,13 @@
-
 export async function getPodcasts() {
-    return await fetch('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false')
+  return await fetch(
+    "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false"
+  )
     .then((data) => data.json())
-        .then((json) => json)
-        .catch((error) => {
-            console.error('nått blev fel:', error)
-            return null;
-        })
-
+    .then((json) => json)
+    .catch((error) => {
+      console.error("nått blev fel:", error);
+      return null;
+    });
 }
 
 export default getPodcasts;
