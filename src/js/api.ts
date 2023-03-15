6cc66c4ec6fe8126a53751ee43b37b7b05d59c61
MarcusRei <marcusreineck@gmail.com>
2023-03-15 11:56:37 +0100

    add format script, format code

--- src/js/api.ts
+++ src/js/api.ts
@@ -1,11 +1,11 @@
 export async function getPodcasts() {
   return await fetch(
-    "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false"
+    'https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false'
   )
-    .then((data) => data.json())
-    .then((json) => json)
-    .catch((error) => {
-      console.error("nått blev fel:", error);
+    .then(data => data.json())
+    .then(json => json)
+    .catch(error => {
+      console.error('nått blev fel:', error);
       return null;
     });
 }
--- src/js/toggleDarkmode.ts
+++ src/js/toggleDarkmode.ts
@@ -1,15 +1,15 @@
-const toggleLightModeButton = document.querySelector(".toggle-btn");
-toggleLightModeButton.addEventListener("click", toggleLightMode);
+const toggleLightModeButton = document.querySelector('.toggle-btn');
+toggleLightModeButton.addEventListener('click', toggleLightMode);
 //console.log(toggleLightModeButton);
 
 export function toggleLightMode() {
-  document.body.classList.toggle("darkmode");
-  if (document.body.classList.contains("darkmode")) {
-    toggleLightModeButton.innerHTML = "Välj mörkt läge";
-    console.log("mörkt läge");
+  document.body.classList.toggle('darkmode');
+  if (document.body.classList.contains('darkmode')) {
+    toggleLightModeButton.innerHTML = 'Välj mörkt läge';
+    console.log('mörkt läge');
   } else {
-    toggleLightModeButton.innerHTML = "Välj ljust läge";
-    console.log("ljust läge");
+    toggleLightModeButton.innerHTML = 'Välj ljust läge';
+    console.log('ljust läge');
   }
 }
 
