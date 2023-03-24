82235315a9c9cc603344ec7f05e8c86267f7f35b
Marcus Reineck <100363230+MarcusRei@users.noreply.github.com>
2023-03-24 15:09:01 +0100

    (bug) remove second return in fetch

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -4,7 +4,6 @@ let apiEndpoint: string = import.meta.env.VITE_APP_SR_API_ENDPOINT;
 export async function getPodcasts() {
   return await fetch(apiEndpoint)
     .then(data => data.json())
-    .then(json => json)
     .catch(error => {
       console.error('nått blev fel:', error);
       return null;
